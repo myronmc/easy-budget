0536eebdfa3b7294edc66b549ab817525e86b1cb
Byron <byronbuckley@live.com>
2013-12-18 01:50:30 -0500

    initialised income controller added: - add income - delete income - undo income - started edit income - started charts integration refactored JS to use individual files

--- app/js/mvc/app.js
+++ app/js/mvc/app.js
@@ -1,21 +1,54 @@
 'use strict';
+var totalIncome = 10;
+var totalExpense = 20; 
 
-var easyBudget = angular.module('easyBudget', []);
+$(document).foundation(); //initialize foundation
 
-var budgetEntry = function (desc, value) {
-	this.desc = desc;
-	this.value = value;
-};
+ // Load the Visualization API and the piechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
 
-easyBudget.controller('IncomeCtrl', ['$scope', function ($scope) {
-  // Controller magic
-	$scope.addIncome = function() { 
-  	  var incomeArr = [];
-	  var incomeRecord = new budgetEntry($scope.addIncomeDesc, $scope.addIncomeValue);
-	  incomeArr.push(incomeRecord);
-	  console.log(incomeArr[0].addIncomeDesc + ", " + incomeArr[0].addIncomeValue);
-	};
-}]);
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+    //updateChartValues();
+    
+    // Create the data table.
+    var data = google.visualization.arrayToDataTable([
+    ['Income $', 'Expenses  $'],
+    ['Income $', totalIncome],
+    ['Expense $', totalExpense]
+  ]);
+
+    // Set chart options
+    var options = {'title':'',
+                   chartArea:{left:20,top:20,width:'100%',height:'100%'},
+                   'width': 250,
+                   'height': 300, 
+                   'pieSliceText': 'value',
+                   'legend': 'top',                    
+                   colors: ['#43ac6a', '#ea2f10']};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);    
+  }  
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getBudgetTotal (budgetArr) {
+	var budgetTotal = 0;
+	for (var i = 0; i < budgetArr.length; i++) {
+		budgetTotal += budgetArr[i].value;
+	}
+	return budgetTotal;
+}
 
 // Declare app level module which depends on filters, and services
 /*
--- app/js/mvc/controllers.js
+++ app/js/mvc/controllers.js
@@ -1,11 +1,66 @@
 'use strict';
 
+/*
+ * classes
+ */
+var budgetEntry = function (desc, value) {
+	this.desc = desc;
+	this.value = value;
+	this.setDesc = function(newValue) {
+		this.desc = newValue;
+	};
+	this.setValue = function(newValue) {
+		this.value = newValue;
+	};
+};
+
+
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+var easyBudget = angular.module('easyBudget', []);
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+easyBudget.controller('IncomeCtrl', ['$scope', function ($scope) {
+  	$scope.incomeArr = [];
+	var incomeUndoBuffer = [];
+  	
+  	//generate income records on 1st load	
+	var incomeRecords = getRandomInt(1,5);	
+	for (var i = 0; i < incomeRecords; i++) {
+		var incomeRecord = new budgetEntry("Income " + (i+1), getRandomInt(1,100));
+	  	$scope.incomeArr.push(incomeRecord);
+	}
+	totalIncome = getBudgetTotal($scope.incomeArr);	  
+	drawChart();
+	
+	$scope.addIncome = function() {   	  
+	  var incomeRecord = new budgetEntry($scope.addIncomeDesc, $scope.addIncomeValue);
+	  $scope.incomeArr.push(incomeRecord);
+	  
+	  $scope.addIncomeDesc = "";
+	  $scope.addIncomeValue = "";
+	  
+	  totalIncome = getBudgetTotal($scope.incomeArr);	  
+	  drawChart();	
+	  
+	  console.log("IncomeArr Length: " + $scope.incomeArr.length);
+	};
+	
+	$scope.editIncome = function(recordPosition) { 
+		console.log("1. Edit: " + $scope.incomeArr[recordPosition].desc + ", " + $scope.incomeArr[recordPosition].value)
+		$scope.incomeArr[recordPosition].setDesc($scope.editIncomeDesc);
+		$scope.incomeArr[recordPosition].setValue($scope.editIncomeValue);
+		console.log("2. Edit: " + $scope.incomeArr[recordPosition].desc + ", " + $scope.incomeArr[recordPosition].value)
+	};
+	
+	$scope.deleteIncome = function(recordPosition) { 
+		incomeUndoBuffer = [$scope.incomeArr[recordPosition]]; //backup record before deleting
+		$scope.incomeArr.splice(recordPosition,1); //delete 1 record		
+		$scope.isUndoDeleteIncome = true; //show undo panel
+		console.log("IncomeArr Length: " + $scope.incomeArr.length);
+	};
+	
+	$scope.undoDeleteIncome = function() {
+		$scope.incomeArr.unshift(incomeUndoBuffer[0]); //undo by adding deleted record to the top of the table
+		$scope.isUndoDeleteIncome = false; //hide undo panel
+	};
+}]);
